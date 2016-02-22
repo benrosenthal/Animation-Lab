0e641044202f766e7f6769202d0cefe084b95158
Ben <benrosenthal664@gmail.com>
2016-02-22 09:19:11 -0500

    Second Commit -- Delete Wish Button Added -- Animation Not completely smooth enough

--- Ben Rosenthal -- Animation Lab/app/src/main/java/generalassembly/yuliyakaleda/startercode/MainActivity.java
+++ Ben Rosenthal -- Animation Lab/app/src/main/java/generalassembly/yuliyakaleda/startercode/MainActivity.java
@@ -10,6 +10,7 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ListView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import java.util.LinkedList;
 
@@ -17,9 +18,11 @@ public class MainActivity extends AppCompatActivity {
 
   EditText mEnterWishEdittext;
   Button mAddAWishButton;
+  Button mDeleteAWishButton;
   TextView mPreviousWishTextview;
-  ListView mWishesListview;
   LinkedList<String> mWishesList;
+  ListView mWishesListview;
+  ArrayAdapter<String> mWishAdapter;
 
   @Override
   protected void onCreate(Bundle savedInstanceState) {
@@ -29,31 +32,52 @@ public class MainActivity extends AppCompatActivity {
     //TODO: set up all the view and event listeners.
     mEnterWishEdittext = (EditText)findViewById(R.id.enter_wish_edittext);
     mAddAWishButton = (Button)findViewById(R.id.add_a_wish_button);
+    mDeleteAWishButton = (Button)findViewById(R.id.delete_a_wish_button);
     mPreviousWishTextview = (TextView)findViewById(R.id.previous_wish_textview);
-    mWishesListview = (ListView)findViewById(R.id.wishes_listview);
     mWishesList = new LinkedList<String>();
+    mWishesListview = (ListView)findViewById(R.id.wishes_listview);
+
 
     mAddAWishButton.setOnClickListener(new View.OnClickListener() {
         @Override
         public void onClick(View v) {
             //button clicked
             //textview text changes
-            //textview text rotates in animation runs where textview is
+            //textview text rotates in animation_add runs where textview is
             //textview text added to list
-            //animation ends
+            //animation_add ends
             //textview text set to recently added list
             mPreviousWishTextview.setText(mEnterWishEdittext.getText().toString());
             //Animation
-            Animation wishAnimation = AnimationUtils.loadAnimation(MainActivity.this, R.anim.animation);
+            Animation wishAnimation = AnimationUtils.loadAnimation(MainActivity.this, R.anim.animation_add);
             mAddAWishButton.startAnimation(wishAnimation);
-            ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>(MainActivity.this,android.R.layout.simple_list_item_1, mWishesList);
+            mWishAdapter = new ArrayAdapter<String>(MainActivity.this,android.R.layout.simple_list_item_1, mWishesList);
             mWishesList.add(mEnterWishEdittext.getText().toString());
-            mWishesListview.setAdapter(arrayAdapter);
-            arrayAdapter.notifyDataSetChanged();
+            mWishesListview.setAdapter(mWishAdapter);
+            mWishAdapter.notifyDataSetChanged();
             mEnterWishEdittext.setText("");
 
         }
     });
+
+  mDeleteAWishButton.setOnClickListener(new View.OnClickListener() {
+      @Override
+      public void onClick(View v) {
+          if (mWishesList.size() >= 1) {
+              mWishesList.remove(mWishesList.getLast());
+              mWishAdapter.notifyDataSetChanged();
+              if (mWishesList.size() == 0) {
+                  mPreviousWishTextview.setText("");
+              } else {
+                  mPreviousWishTextview.setText(mWishesList.getLast());
+              }
+          } else {
+              Toast.makeText(MainActivity.this, "Add A Wish. Then Delete It", Toast.LENGTH_SHORT).show();
+          }
+      }
+  });
+
+
   }
 
 
